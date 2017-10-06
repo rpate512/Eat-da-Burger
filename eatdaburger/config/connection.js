cd0660536245c90710212ba953d950b8d95db606
Ryan Pate <ryanpate@ryans-mbp.attlocal.net>
2017-10-05 19:25:15 -0500

    New Git, all pages updated

---
+++ eatdaburger/config/connection.js
@@ -0,0 +1,11 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '1challenge',
+  database:'burgers_db'
+});
+
+module.exports=connection;
